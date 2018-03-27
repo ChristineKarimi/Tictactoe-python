10e09156dd284274c5c7a4dbfc82be77f32c3577
christinekarimi <karimichristine@ymail.com>
2018-03-27 14:38:25 +0300

    Added readme

--- tictactoe.py
+++ tictactoe.py
@@ -1,36 +1,87 @@
-#The tic tac toe board
-import random
+def tic_tac_toe():
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end = False
+    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 
-board  =  [0,1,2,
-            3,4,5,
-            6,7,8]
+    def draw():
+        print(board[0], board[1], board[2])
+        print(board[3], board[4], board[5])
+        print(board[6], board[7], board[8])
+        print()
 
-#show function to format the board
+    def p1():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p1()
+        else:
+            board[n] = "X"
 
-def show():
-    
-    
-    print(board[0], board[1], board[2])
-    
-    print(board[3], board[4], board[5])
+    def p2():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p2()
+        else:
+            board[n] = "O"
 
-    print(board[6], board[7], board[8])
-    print() 
-show()
+    def choose_number():
+        while True:
+            while True:
+                a = input()
+                try:
+                    a  = int(a)
+                    a -= 1
+                    if a in range(0, 9):
+                        return a
+                    else:
+                        print("\nThat's not on the board. Try again")
+                        continue
+                except ValueError:
+                   print("\nThat's not a number. Try again")
+                   continue
 
+    def check_board():
+        count = 0
+        for a in win_commbinations:
+            if board[a[0]] == board[a[1]] == board[a[2]] == "X":
+                print("Player 1 Wins!\n")
+                print("Congratulations!\n")
+                return True
 
-while True:
+            if board[a[0]] == board[a[1]] == board[a[2]] == "O":
+                print("Player 2 Wins!\n")
+                print("Congratulations!\n")
+                return True
+        for a in range(9):
+            if board[a] == "X" or board[a] == "O":
+                count += 1
+            if count == 9:
+                print("The game ends in a Tie\n")
+                return True
 
-        input = raw_input('select a spot:')
-        input = int(input)
+    while not end:
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Player 1 choose where to place a cross")
+        p1()
+        print()
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Player 2 choose where to place a nought")
+        p2()
+        print()
 
-if board[input] != 'x' and board[input] = !'o':
-     board[input] = 'x'
+    if input("Play again (y/n)\n") == "y":
+        print()
+        tic_tac_toe()
 
-     random.seed()  #gives a random generator
-     opponent = random.randint = (0,8)
+tic_tac_toe()
 
-            else:
-                print 'This spot is taken'
-                show()
-    
+         
+
+          
